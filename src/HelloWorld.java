ae7160d58427662bc4655c400a5be55267161ea1
msedkiewicz <magdalenainspirations@gmail.com>
2025-11-27 22:44:20 +0100

    add first class

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String args[]) { // String args[] -
+        System.out.println("Hello world!");
+        System.out.println("Hello, Lena!");
+        System.out.println("Hello student!");
+
+        // println moves to a new line
+        // print does not move to a new line
+    }
+}
