e7404a3527a7f4dfebc213b8229a9450e07e7191
msedkiewicz <magdalenainspirations@gmail.com>
2025-11-27 23:07:09 +0100

    Formatting text exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,14 @@ public class HelloWorld {
         System.out.println("Hello world!");
         System.out.println("Hello, Lena!");
         System.out.println("Hello student!");
+        System.out.print("Lena is ");
+        System.out.print("18 years old!");
+        System.out.print("\n5 + 3 equals " + (5 + 3) + ".");
+        System.out.println("\nI'm learning");
+        System.out.println("Java!");
+        System.out.println("1 | \t Name \t Age \t Country");
+        System.out.println("2 | \t Alice \t 25 \t USA");
+        System.out.println("3 | \t Bob \t 30 \t Uk");
 
         // println moves to a new line
         // print does not move to a new line
