7c2869dd4fdc001e712339354ec70e2dddaeffc0
msedkiewicz <magdalenainspirations@gmail.com>
2025-11-27 23:10:48 +0100

    Code formatting

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,11 +6,10 @@ public class HelloWorld {
         System.out.print("Lena is ");
         System.out.print("18 years old!");
         System.out.print("\n5 + 3 equals " + (5 + 3) + ".");
-        System.out.println("\nI'm learning");
-        System.out.println("Java!");
-        System.out.println("1 | \t Name \t Age \t Country");
-        System.out.println("2 | \t Alice \t 25 \t USA");
-        System.out.println("3 | \t Bob \t 30 \t Uk");
+        System.out.println("\nI'm learning\nJava");
+        System.out.println("1 | Name\tAge\t Country");
+        System.out.println("2 | Alice\t25\t USA");
+        System.out.println("3 | Bob\t\t30\t Uk");
 
         // println moves to a new line
         // print does not move to a new line
